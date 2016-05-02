38d76f58a8722ec0d7581ddcd7326484cbfa8451
leonj1 <leonj1@gmail.com>
2016-05-02 11:30:35 -0400

    fixing comment

--- token.py
+++ token.py
@@ -8,11 +8,11 @@ def main(argv):
    try:
       opts, args = getopt.getopt(argv,"hi:o:",["ifile="])
    except getopt.GetoptError:
-      print 'test.py -i <inputfile>'
+      print 'token.py -i <inputfile>'
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print 'test.py -i <inputfile>'
+         print 'token.py -i <inputfile>'
          sys.exit()
       elif opt in ("-i", "--ifile"):
          inputfile = arg
