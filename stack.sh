7e9895352faf9589e4700cc30dc29931d287ede7
leonj1 <leonj1@gmail.com>
2016-05-04 09:18:27 -0400

    refactoring stack a little bit. Mostly OCD stuff

--- stack.sh
+++ stack.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-SRC_CLUSTER="feature/one"
-TARGET_CLUSTER=provisioning/envs/devops/USERNAME
+ATLAS_BASE=~/src/atlas/provisioning/envs
+SRC_CLUSTER=$ATLAS_BASE/feature/one
+TARGET_CLUSTER=$ATLAS_BASE/devops/jleon
 
 STAMP_PATH=~/workarea/stamp
 VAULT_FILE=~/.secrets/vault
@@ -11,7 +12,7 @@ if [ ! -r $VAULT_FILE ]; then
     exit 1
 fi
 
-cp -r ./provisioning/envs/$SRC_CLUSTER $TARGET_CLUSTER
+cp -r $SRC_CLUSTER $TARGET_CLUSTER
 ansible-vault decrypt --vault-password-file=$VAULT_FILE $TARGET_CLUSTER/secrets.yml
 
 python $STAMP_PATH/token.py -i $STAMP_PATH/template.json
